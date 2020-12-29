1f658d24584a18569e335b19f756615a9a0681c4
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-30 01:35:22 +0600

    Update Program.cs
    //Написать программу, которая при старте дописывает текущее время в файл
    //«startup.txt».

--- startup/Program.cs
+++ startup/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+//Написать программу, которая при старте дописывает текущее время в файл
+//«startup.txt».
 
 namespace startup
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {  
+            StreamWriter userData2 = new StreamWriter("startup.txt", true);
+            userData2.WriteLine(DateTime.Now.ToShortTimeString());
+            userData2.Close();
         }
     }
 }
